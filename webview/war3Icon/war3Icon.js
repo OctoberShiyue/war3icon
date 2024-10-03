f224a80a8ae06a45eebc4e3ca01a1df415231456
十月 <1023261581@qq.com>
2024-10-03 18:02:44 +0800

    1

--- webview/war3Icon/war3Icon.js
+++ webview/war3Icon/war3Icon.js
@@ -26,15 +26,6 @@ function toggleType(btn) {
 		iterator.className = 'texture-type';
 	}
 	btn.className = 'texture-type selected';
-	let textureContainer = document.getElementById("texture-content");
-	let itemTextureContainer = document.getElementById("item-texture-content");
-	if (btn.value === '技能') {
-		textureContainer.style.display = 'grid';
-		itemTextureContainer.style.display = 'none';
-	} else if (btn.value === '物品') {
-		textureContainer.style.display = 'none';
-		itemTextureContainer.style.display = 'grid';
-	}
 }
 function heroFilter(img, heroName) {
 	const filter = document.getElementById("filter");
@@ -46,18 +37,6 @@ function heroFilter(img, heroName) {
 	const heroFilter = document.getElementById("hero-filter");
 	heroFilter.src = img.src;
 	onInput();
-}
-function copyIconName(iconName) {
-	vscode.postMessage({
-		type: 'copy_ability_name',
-		text: iconName
-	});
-}
-function openFolder(iconName) {
-	vscode.postMessage({
-		type: 'copy_ability_file',
-		text: iconName
-	});
 }
 document.oncontextmenu = function (event) {
 	event.preventDefault();
