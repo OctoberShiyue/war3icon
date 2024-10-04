7c490577fcea8c11e781acda38f2b877c2c85863
十月 <1023261581@qq.com>
2024-10-04 13:58:49 +0800

    1

--- src/command/cmdWar3IconPanel.ts
+++ src/command/cmdWar3IconPanel.ts
@@ -3,7 +3,9 @@ import * as fs from 'fs';
 import { Uri } from "vscode";
 import * as path from 'path';
 import { exec } from 'child_process';
+import axios from 'axios';
 import { getWebviewContent } from '../utils/getWebViewContent';
+
 
 export async function war3IconPanel(context: vscode.ExtensionContext) {
 	// 创建一个Webview视图
@@ -17,14 +19,59 @@ export async function war3IconPanel(context: vscode.ExtensionContext) {
 		}
 	);
 
-	// 页面内容
-	panel.webview.html = await getWebviewContent(panel.webview, context.extensionUri, "war3Icon", html => {
-		return html;
-	});
+	let datalist: any[] = [];
+	let last_value: string = "";
+	async function update_html_data(value: string = "", page: number = 1) {
+		if (last_value !== value) {
+			datalist = [];
+		}
+		last_value = value;
+		// 页面内容
+		panel.webview.html = await getWebviewContent(panel.webview, context.extensionUri, "war3Icon", html => {
+			const response = axios.get("http://war3.newxd.cn/api/demo/getImagerList?limit=200&like=" + value + "&page=" + page, {
+				headers: {
+					'Content-Type': 'application/json',
+				}
+			});
+			response.then(async response => {
+				datalist[page - 1] = response.data.data.data;
+				let replaceText = "";
+				for (let index = 0; index < datalist.length; index++) {
+					const element = datalist[index];
+					if (element) {
+						for (let index = 0; index < element.length; index++) {
+							const element2 = element[index];
+							let url = "http://war3.newxd.cn" + element2.url;
+							let filename = element2.filename;
+							replaceText += `\t\t\t\t\t\t<img class="item-texture-icon" id = "items" src=` + url + ` alt='${filename}' onclick="filter(this,'${filename}')">\n`;
+						}
+					}
+				}
+				console.log("更新数据", value, page,response.data.data.last_page);
+
+				panel.webview.postMessage({ type: 'updateitems', text: replaceText , last_page:response.data.data.last_page });
+			});
+			return html;
+		});
+
+	}
+	update_html_data("");
 
 	// 监听消息
-	panel.webview.onDidReceiveMessage(async (message: { type: string, text: string; }) => {
-		
+	panel.webview.onDidReceiveMessage(async (message: { type: string, text: string; page: number }) => {
+		console.log(message);
+
+		const type = message.type;
+		const text = message.text;
+		const page = message.page;
+
+		switch (type) {
+			case "update_html_data":	// 复制技能名
+				update_html_data(text, page);
+				panel.webview.postMessage({ type: 'updateitemsfull' });
+				return;
+		}
+
 	}, null, context.subscriptions);
 	// 销毁处理
 	panel.onDidDispose(() => {
--- webview/war3Icon/war3Icon.js
+++ webview/war3Icon/war3Icon.js
@@ -1,13 +1,34 @@
 // Script run within the webview itself.
 const vscode = acquireVsCodeApi();
-window.addEventListener("load", function () {
-	updateData("");
-});
 
-function updateData(value) {
-	console.log(value);
-	const content = document.getElementById("texture-content");
-	content.innerHTML = value;
+let now_page = 1;
+let now_value;
+let is_update;
+
+function updateScroll() {
+	// 获取页面滚动的高度
+	var scrollTop = $(window).scrollTop();
+	// 获取窗口高度
+	var windowHeight = $(window).height();
+	// 获取文档的总高度
+	var documentHeight = $(document).height();
+	// 判断是否滚动到底部
+	if (scrollTop + windowHeight >= documentHeight - 20 && Date.now() - timestamp >= 100 && !is_update) {
+		timestamp = Date.now();
+		now_page = now_page + 1;
+		updateData(now_value, now_page);
+	}
+}
+
+function updateData(value, page = 1) {
+	now_value = value;
+	now_page = page;
+	is_update = true;
+	vscode.postMessage({
+		type: 'update_html_data',
+		text: value,
+		page: page
+	});
 }
 
 function checkEnter(event) {
@@ -31,3 +52,24 @@ function toggleType(btn) {
 document.oncontextmenu = function (event) {
 	event.preventDefault();
 };
+window.addEventListener('message', event => {
+	const message = event.data; // 消息数据
+	if (message.type === 'updateitems') {
+		$("#texture-content").html(message.text);
+		now_page = Math.min(now_page, message.last_page);
+	}
+	if (message.type === 'updateitemsfull') {
+		is_update = null;
+		console.log("重置");
+	}
+});
+let timestamp = Date.now();
+$(window).on('scroll', function () {
+	updateScroll();
+});
+
+$(window).on('wheel', function(event) {
+	if (event.originalEvent.deltaY > 0) {
+		updateScroll();
+	}
+});
